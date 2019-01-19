a2cf8e490ac61363f36776c799303c1cb6dad27e
Andrzej Świątek <andrzej.swiatek00@gmail.com>
2019-01-19 13:57:09 +0100

    Wprowadzenie przykładowego projektu wyszukującego narożniki w obrazie.

---
+++ gaussian.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import ndimage as ndi
+
+from skimage import feature, io
+
+# Generate noisy image of a square
+#im = np.zeros((128, 128))
+#im[32:-32, 32:-32] = 1
+#im = ndi.rotate(im, 15, mode='constant')
+#im = ndi.gaussian_filter(im, 4)
+#im += 0.2 * np.random.random(im.shape)
+
+im = io.imread('images/tasma.png', as_gray=True)
+# Compute the Canny filter for two values of sigma
+edges1 = feature.canny(im)
+edges2 = feature.canny(im, sigma=2)
+edges3 = feature.canny(im, sigma=3)
+edges4 = feature.canny(im, sigma=4)
+
+edges = [edges1, edges2, edges3, edges4]
+# display results
+fig, axes = plt.subplots(nrows=1, ncols=len(edges), figsize=(8, 3),
+                                    sharex=True, sharey=True)
+
+axes[0].imshow(im, cmap=plt.cm.gray)
+axes[0].axis('off')
+axes[0].set_title('noisy image', fontsize=20)
+
+for e in range(1, len(edges)):
+    axes[e].imshow(edges[e], cmap=plt.cm.gray)
+    axes[e].axis('off')
+    axes[e].set_title('Canny filter, $\sigma=1$', fontsize=20)
+
+
+fig.tight_layout()
+
+plt.show()
---
+++ sobel.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from skimage.data import camera
+from skimage.filters import roberts, sobel, scharr, prewitt
+from skimage import io
+
+#image = camera()
+image = io.imread('images/tasma.png', as_gray=True);
+edge_roberts = prewitt(image)
+edge_sobel = sobel(image)
+
+fig, ax = plt.subplots(ncols=3, sharex=True, sharey=True,
+                       figsize=(8, 4))
+
+ax[0].imshow(image, cmap=plt.cm.gray)
+ax[0].set_title('Input image')
+
+ax[1].imshow(edge_roberts, cmap=plt.cm.gray)
+ax[1].set_title('Roberts Edge Detection')
+
+ax[2].imshow(edge_sobel, cmap=plt.cm.gray)
+ax[2].set_title('Sobel Edge Detection')
+
+for a in ax:
+    a.axis('off')
+
+plt.tight_layout()
+plt.show()
